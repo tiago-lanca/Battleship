b5c4da34b630b1ad530f46e7d9697df2e88c776d
Tiago Lança <tiagofilipe.lanca@gmail.com>
2025-03-28 02:17:54 +0000

    Automated inputs for testing

--- Battleship/Program.cs
+++ Battleship/Program.cs
@@ -13,12 +13,28 @@ namespace Battleship
             GameController controller = new GameController();
             string command;
 
+            // Executing automated inputs for testing 
+            var inputs = new StringReader("RJ tiago\nRJ diogo\nIJ tiago diogo\nCN tiago L 1 A\nXO");
+            Console.SetIn(inputs);
+
+            int automatedInputs = 0;
+
+            while (automatedInputs < 5 && (command = view.GetCommand()) != null)  
+            {                
+                Console.WriteLine($"{command}");
+                controller.CheckCommand(command, view, playersList);
+                automatedInputs++;
+            }
+            Console.SetIn(new StreamReader(Console.OpenStandardInput()));
+            // ------------------------------------------------------  //
+
             do
             {
                 command = view.GetCommand();
                 controller.CheckCommand(command, view, playersList);
             }
             while (!string.IsNullOrWhiteSpace(command));
+
         }
     }
 }

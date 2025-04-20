8f2992bbc7edb5e3ace70ea317636037a9c31b50
Tiago Lança <tiagofilipe.lanca@gmail.com>
2025-04-20 02:23:49 +0100

    gitignore updated

--- obj/Debug/net8.0/Battleship.AssemblyInfo.cs
+++ obj/Debug/net8.0/Battleship.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Battleship")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1815125545d1f4f60dfa58ccb7b10716ac1999d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+404270a0768cbda901c62d877a1dcfe8053c649f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Battleship")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Battleship")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
